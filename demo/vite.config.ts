b95e64f504f6f8e81d8c31cf2e860977a057e3a2
Kouji Matsui <k@kekyo.net>
2025-09-24 11:44:59 +0900

    feat: Adjusted project scripts.

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     react()
   ],
-  resolve: {
-    alias: [
-      { find: 'child-locator', replacement: resolve(__dirname, '../child-locator/src/index.ts') },
-      { find: 'child-locator/', replacement: resolve(__dirname, '../child-locator/src/') },
-    ],
-  },
   server: {
     port: 59517,
     hmr: {
